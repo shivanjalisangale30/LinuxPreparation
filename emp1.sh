e2e5306e8523fb181d3c78c7be916701b1214227
shivanjalisangale30 <53612250+shivanjalisangale30@users.noreply.github.com>
2019-11-09 17:24:23 +0530

    Employee daily wage add in program

--- emp1.sh
+++ emp1.sh
@@ -1,8 +1,19 @@
-#!/bin/bash -x
-present=$(( RANDOM % 2))
-if [ $present==1 ]
+#!/bin/bash -x                                                                              
+randomcheck=$(( RANDOM % 3 ))
+RateperHr=10;
+isfulltime=1;
+isparttime=0;
+if [ $randomchek -eq $isfulltime ];
 then
-echo "Employee is present"
-else [ $present==0 ]
+Hrs=5;
+echo "Empolee is present"
+elif [ $randomcheck -eq $isparttime ]
+then
+Hrs=6;
+echo "Part time"
+else
 echo "Employee is absent"
 fi
+salary=$(($randomcheck * $Hrs))
+echo $salary
+
