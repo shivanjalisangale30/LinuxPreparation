d80dfcfa847bdfb477be2fa1d7a400ccc6e91e42
blsonalib <blsonalib@gmail.com>
2019-11-11 18:09:30 +0530

    pushing 4 wala

--- operation.sh
+++ operation.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -x
-read -p "Enter your choice"
+read -p "Enter below your choice"
 read -p "Enter First Number": a 
 read -p "Enter Second Number": b
 read -p "Enter Thrird Nuber": c
@@ -9,3 +9,5 @@ operation2=$(($a*$b+$c))
 echo "Your result 2  is": $operation2
 operation3=$(($c+$a/$b))
 echo "Your result 3  is": $operation3
+operation4=$(($a%$b+$c))
+echo "Your result 4  is": $operation4\
