3fd86099d65768061e90d2930273ba34029ed5a3
blsonalib <blsonalib@gmail.com>
2019-11-11 18:51:14 +0530

    Pushing 5 wala in opearation

--- operation.sh
+++ operation.sh
@@ -1,13 +1,19 @@
 #!/bin/bash -x
+count=0;
 read -p "Enter below your choice"
 read -p "Enter First Number": a 
 read -p "Enter Second Number": b
 read -p "Enter Thrird Nuber": c
-opeartion1=$(($a+$b*$c))
-echo "Your result 1  is": $opeartion1
-operation2=$(($a*$b+$c))
-echo "Your result 2  is": $operation2
-operation3=$(($c+$a/$b))
-echo "Your result 3  is": $operation3
-operation4=$(($a%$b+$c))
-echo "Your result 4  is": $operation4
+
+
+declare -A results
+results[opeartion1]=$(($a+$b*$c))
+results[operation2]=$(($a*$b+$c))
+results[operation3]=$(($c+$a/$b))
+results[operation4]=$(($a%$b+$c))
+
+echo "Your result 1  is": ${results[opeartion1]}
+echo "Your result 2  is": ${results[operation2]}
+echo "Your result 3  is": ${results[operation3]}
+echo "Your result 4  is": ${results[operation4]}
+
