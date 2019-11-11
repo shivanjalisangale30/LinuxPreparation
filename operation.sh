fd7afacc97c6b0e308421a94ccadccb401f6bfc2
blsonalib <blsonalib@gmail.com>
2019-11-11 17:47:16 +0530

    Pushing to 2 wala

--- operation.sh
+++ operation.sh
@@ -3,5 +3,5 @@ read -p "Enter your choice"
 read -p "Enter First Number": a 
 read -p "Enter Second Number": b
 read -p "Enter Thrird Nuber": c
-opeartion=$(($a+$b*$c))
-echo "Your result is": $opeartion
+operation2=$(($a*$b+$c))
+echo "Your result is": $operation2
